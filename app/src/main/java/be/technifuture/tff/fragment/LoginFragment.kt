85e9639dc840ea9f2e3213d9031ad4352bcc7cb8
tony <izzi.tony@gmail.com>
2023-09-21 11:34:39 +0200

    Remise ne ordre des Import ?

--- app/src/main/java/be/technifuture/tff/fragment/LoginFragment.kt
+++ app/src/main/java/be/technifuture/tff/fragment/LoginFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import be.technifutur.tlm.tff.databinding.FragmentLoginBinding
+import be.technifuture.tff.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
