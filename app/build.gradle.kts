f5814b48cd20e6d7f59120b5494fc978755dfadd
tony <izzi.tony@gmail.com>
2023-10-26 10:29:15 +0200

    Import des dépendence de navigation + De rooms qui paurria etre util plus tard.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import com.android.build.api.dsl.BuildFeatures
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -51,4 +53,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    val roomVersion = "2.5.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:$roomVersion")
 }
