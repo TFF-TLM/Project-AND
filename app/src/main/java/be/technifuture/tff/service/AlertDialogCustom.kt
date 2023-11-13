df0326cb3628694e208b7cc0a78f1dc1b0e49fc6
tony <izzi.tony@gmail.com>
2023-11-13 13:47:26 +0100

    Mise en form du fichier string

--- app/src/main/java/be/technifuture/tff/service/AlertDialogCustom.kt
+++ app/src/main/java/be/technifuture/tff/service/AlertDialogCustom.kt
@@ -20,8 +20,6 @@ class AlertDialogCustom (private val viewContext: Context){
 
     fun getAlert(error: ErrorValidation){
 
-        //val binding = AlertDialogueCustomBinding.inflate(LayoutInflater.from(viewContext))
-
         val builder = androidx.appcompat.app.AlertDialog.Builder(viewContext, R.style.AlertDialogCustom)
             .create()
         val view = LayoutInflater.from(viewContext).inflate(R.layout.alert_dialogue_custom,null)
@@ -49,38 +47,8 @@ class AlertDialogCustom (private val viewContext: Context){
             ErrorValidation.LOG_ERROR ->
                 view.resources.getString(R.string.LOG_ERROR)
         }
-
-
-
-
         builder.setCanceledOnTouchOutside(false)
         builder.show()
-
-
-
-       /* val alert = AlertDialog.Builder(viewContext)
-        alert.setTitle("Error")
-        alert.setMessage(
-            when(error){
-                ErrorValidation.LOGIN_EXIST ->
-                    R.string.LOGIN_EXIST
-                ErrorValidation.LOGIN_RULES ->
-                    R.string.LOGIN_RULES
-                ErrorValidation.PASSWORD_RULES ->
-                    R.string.PASSWORD_RULES
-                ErrorValidation.PASSWORD_SAME ->
-                    R.string.PASSWORD_SAME
-                ErrorValidation.MAIL_EXIST ->
-                    R.string.MAIL_EXIST
-                ErrorValidation.MAIL_RULES ->
-                    R.string.MAIL_RULES
-                ErrorValidation.LOG_ERROR ->
-                    R.string.LOG_ERROR
-            })
-        alert.setNeutralButton("Ok", null)
-        alert.create()
-        alert.show()
-        */
     }
 
 }
